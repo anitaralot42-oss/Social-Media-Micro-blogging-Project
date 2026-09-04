43d8884bdbe0c466ccc5c5c01d9e9a4c3c1b8466
Your Name <your-email@example.com>
2026-09-05 00:17:49 +0530

    prepare backend for vercel

--- ConnectPluse/backend/server.js
+++ ConnectPluse/backend/server.js
@@ -19,8 +19,4 @@ app.use('/api/auth', require('./routes/authRoutes'))
 app.use('/api/posts', require('./routes/postRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
-})
+module.exports = app
